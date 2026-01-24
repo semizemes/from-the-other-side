53cbd615cd6fb96ba36c5ebb996ba70b11908ad1
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-24 00:13:43 +0000

    feat: implement serveStatic function and update server to use it

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-import http from "node:http"
+import http from "node:http" 
+import { serveStatic } from "./utils/serveStatic.js";
 
 const PORT = 8000
 
+const __dirname = import.meta.dirname
+console.log(__dirname);
+
+
 const server = http.createServer((req, res) => {
+    serveStatic(__dirname)
     res.setHeader("Content-Type", "text/html")
     res.statusCode = 200
-
     res.end("<html><h1>The server is working</h1></html>")
 })
 
---
+++ utils/serveStatic.js
@@ -0,0 +1,6 @@
+import path from "node:path"
+
+export function serveStatic(baseDir) {
+    const filePath = path.join(baseDir, "public", "index.html")
+    console.log(filePath)
+}
