cafb5f9284211e1aa9b4755a9c0e3ea425227afe
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-24 23:53:24 +0000

    feat: enhance serveStatic function to dynamically determine content type and add getContentType utility

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,16 +1,18 @@
 import path from "node:path"
 import fs from "node:fs/promises"
 import { sendResponse } from "./sendResponse.js"
+import { getContentType } from "./getContentType.js"
 
 export async function serveStatic(req, res, baseDir) {
 
-    const filePath = path.join(baseDir, "public", "index.html")
+    const filePath = path.join(baseDir, "public", req.url === "/" ? "index.html" : req.url )
+    const ext = path.extname(filePath)
 
-    console.log(filePath)
     try{
         const content = await fs.readFile(filePath)
-        sendResponse(res, 200, "text/html", content)
+        sendResponse(res, 200, getContentType(ext), content)
     } catch (err){
         console.log(err)
+        
     }
 }
