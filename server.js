f5213d1e75404c3442098b4cf9e8bf723ca93dec
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-25 01:42:48 +0000

    feat: add getData utility and integrate it into server logging feat: create data.json with initial ghost story entries

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import http from "node:http";
 import { serveStatic } from "./utils/serveStatic.js";
+import { getData } from "./utils/getData.js";
 
 const PORT = 8000;
 
 const __dirname = import.meta.dirname;
-console.log(__dirname);
+
+console.log(getData())
 
 const server = http.createServer(async (req, res) => {
   await serveStatic(req, res, __dirname);
---
+++ utils/getData.js
@@ -0,0 +1,7 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+
+export function getData() {
+
+  return 'I am from getData()!'
+}
