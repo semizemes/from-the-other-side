5e021f06f93c8a4f1f514a77563f8600ebc9d120
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-26 06:12:40 +0500

    feat: implement handlePost function and integrate parseJSONBody utility for POST requests

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,9 +1,16 @@
 import { getData } from "../utils/getData.js";
+import { parseJSONBody } from "../utils/parseJSONBody.js";
 import { sendResponse } from "../utils/sendResponse.js";
 
 export async function handleGet(res) {
-  const response = await getData();
-  const data = JSON.stringify(response);
+  const data = await getData();
+  const content = JSON.stringify(data);
 
-  sendResponse(res, 200, "application/json", data);
+  sendResponse(res, 200, "application/json", content);
+}
+
+export async function handlePost(req, res) {
+  const rawBody = await parseJSONBody(req);
+  console.log(rawBody);
+  return;
 }
