2ec0909d5573dfff6a428bb61a787081f23b96a7
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-02-02 06:55:50 +0500

    refactor: streamline addNewSighting function and remove commented code

--- utils/addNewSighting.js
+++ utils/addNewSighting.js
@@ -3,30 +3,12 @@ import path from "node:path";
 
 export async function addNewSighting(newSighting) {
   try {
-    const __dirname = import.meta.dirname;
-    const baseDir = path.join(__dirname, "..");
-    const dataJSON = path.join(baseDir, 'data', 'data.json')
-    // console.log(dataJSON); //D:\Scrimba\NodeJS\from-the-other-side\data\data.json
-
-    // console.log(newSighting);
-//            {
-//          location: '1231',
-//          timeStamp: '12 March 2004 at 23:13',
-//          text: '12312',
-//          title: 'asd'
-//              }
-    const existingData = await fs.readFile(dataJSON, 'utf8');
-    // console.log(typeof existingData); //bu string data, shunig uchun shunchaki yangi obektni qo'shib bo'lmaydi
-    
-    const JSONArray = JSON.parse(existingData)
-    // console.log(typeof JSONArray); // bu ujee array, shunchun yangi kelgan obejctni bunga push qilish mumkin
-
-    JSONArray.push(newSighting)
-    console.log(JSONArray);
-    
-    
-    
+    const pathJSON = path.join("data", "data.json");
+    const existingData = await fs.readFile(pathJSON, "utf8");
+    const JSONArray = JSON.parse(existingData);
+    JSONArray.push(newSighting);
+    await fs.writeFile(pathJSON, JSON.stringify(JSONArray, null, 2), "utf8");
   } catch (err) {
-
+    throw new Error(err);
   }
 }
