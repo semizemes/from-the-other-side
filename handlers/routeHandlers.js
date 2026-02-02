65598883143b107ada43d95f8e170d2bcab33db9
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-02-02 14:22:32 +0500

    feat: implement live news feed with event streaming and styling updates

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,6 +4,7 @@ import { sendResponse } from "../utils/sendResponse.js";
 import { addNewSighting } from "../utils/addNewSighting.js";
 import { sanitizeInput } from "../utils/sanitizeInput.js";
 import { sightingEvents } from "../events/sightingEvents.js";
+import { stories } from "../data/stories.js";
 
 export async function handleGet(res) {
   const data = await getData();
@@ -21,4 +22,18 @@ export async function handlePost(req, res) {
   } catch (err) {
     sendResponse(res, 401, "application/json", JSON.stringify({ error: err }));
   }
+}
+
+export async function handleNews(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length);
+    res.write(
+      `data: ${JSON.stringify({ event: "hot-news", story: stories[randomIndex] })}\n\n`,
+    );
+  }, 3000);
 }
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import http from "node:http";
 import { serveStatic } from "./utils/serveStatic.js";
-import { handleGet } from "./handlers/routeHandlers.js";
-import { handlePost } from "./handlers/routeHandlers.js";
+import { handleGet, handlePost, handleNews } from "./handlers/routeHandlers.js";
 
 const PORT = process.env.PORT || 8000;
 
@@ -14,6 +13,8 @@ const server = http.createServer(async (req, res) => {
     } else if (req.method == "POST") {
       return await handlePost(req, res);
     }
+  } else if (req.url === "/api/news") {
+    return await handleNews(req, res);
   } else if (!req.url.startsWith("/api")) {
     return await serveStatic(req, res, __dirname);
   }
